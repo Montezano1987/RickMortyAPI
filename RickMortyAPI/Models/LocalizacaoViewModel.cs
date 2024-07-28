216cc265f0a220b58f537f4479201e2e7efbd9c1
montezano1987 <brunovmontezano@gmail.com>
2024-07-28 11:06:43 -0300

    Corrigido ViewModel da localizacao e testado toda a localizacao na web !

--- RickMortyAPI/Models/LocalizacaoViewModel.cs
+++ RickMortyAPI/Models/LocalizacaoViewModel.cs
@@ -1,52 +1,41 @@
-﻿// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+﻿
 using System.Text.Json.Serialization;
 
-public class Info
+namespace RickMortyAPI.Models
 {
-    [JsonPropertyName("count")]
-    public int Count { get; set; }
 
-    [JsonPropertyName("pages")]
-    public int Pages { get; set; }
+    public class LocalizacaoViewModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
 
-    [JsonPropertyName("next")]
-    public string Next { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
 
-    [JsonPropertyName("prev")]
-    public object Prev { get; set; }
-}
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
 
-public class LocalizacaoViewModel
-{
-    [JsonPropertyName("id")]
-    public int Id { get; set; }
+        [JsonPropertyName("dimension")]
+        public string Dimension { get; set; }
 
-    [JsonPropertyName("name")]
-    public string Name { get; set; }
+        [JsonPropertyName("residents")]
+        public List<string> Residents { get; set; }
 
-    [JsonPropertyName("type")]
-    public string Type { get; set; }
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
 
-    [JsonPropertyName("dimension")]
-    public string Dimension { get; set; }
+        [JsonPropertyName("created")]
+        public DateTime Created { get; set; }
+    }
 
-    [JsonPropertyName("residents")]
-    public List<string> Residents { get; set; }
+    public class LocalizacaoResponse
+    {
+        [JsonPropertyName("info")]
+        public Info Info { get; set; }
 
-    [JsonPropertyName("url")]
-    public string Url { get; set; }
-
-    [JsonPropertyName("created")]
-    public DateTime Created { get; set; }
-}
-
-public class LocalizacaoResponse
-{
-    [JsonPropertyName("info")]
-    public Info Info { get; set; }
-
-    [JsonPropertyName("results")]
-    public List<LocalizacaoViewModel> Results { get; set; }
+        [JsonPropertyName("results")]
+        public List<LocalizacaoViewModel> Results { get; set; }
+    }
 }
 
 
