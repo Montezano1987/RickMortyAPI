492d105919a418138010180ef7b9c12016c264e7
montezano1987 <brunovmontezano@gmail.com>
2024-07-28 10:28:27 -0300

    Comecando fazer a localizacao, criado controller

---
+++ RickMortyAPI/Controllers/LocalizacaoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RickMortyAPI.Models;
+using System.Text.Json;
+
+namespace RickMortyAPI.Controllers
+{
+    public class LocalizacaoController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public LocalizacaoController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("https://rickandmortyapi.com/api/location");
+            response.EnsureSuccessStatusCode();
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var localizacaoResponse = JsonSerializer.Deserialize<LocalizacaoResponse>(jsonString, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return View(localizacaoResponse.Results);
+        }
+    }
+}
